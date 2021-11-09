801018c373d8d382df804f25c6128fdd4e779346
only <hwy113329@163.com>
2021-11-09 20:03:05 +0800

    spring学习

---
+++ src/main/java/com/imooc/spring/ioc/entity/Apple.java
@@ -0,0 +1,15 @@
+package com.imooc.spring.ioc.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class Apple {
+    private String title;
+    private String color;
+    private String origin;
+
+}
